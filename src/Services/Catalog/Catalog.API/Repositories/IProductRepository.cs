de9db0debdf9f20a19ec10e3aee617ad88f96f5c
Hamed Khatami <hamedkhatami@outlook.com>
2021-10-27 15:31:58 +0330

    ProductRepository is added

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+	public interface IProductRepository : IRepository<Product>
+	{
+		Task<IEnumerable<Product>> GetProductByName(string name);
+
+		Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
+	}
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+	public interface IRepository<T>
+	{
+		Task<IEnumerable<T>> Get();
+
+		Task Create(T entity);
+
+		Task<bool> Update(T entity);
+
+		Task<bool> Delete(string id);
+
+		Task<T> GetById(string id);
+	}
+}
