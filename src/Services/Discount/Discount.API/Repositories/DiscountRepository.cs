b2fadbf84caa8a261248604093d7be3cc3f41040
hamedkhatami <hamedkhatami@outlook.com>
2022-01-06 18:33:11 +0330

    Feat (DiscountRepository): Implement GetDiscount method

--- src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
+++ src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -1,25 +1,51 @@
-﻿using Discount.API.Entities;
+﻿using Dapper;
+using Discount.API.Entities;
+using Npgsql;
 
 namespace Discount.API.Repositories
 {
 	public class DiscountRepository : IDiscountRepository
 	{
-		public Task<bool> CreateDiscount(Coupon coupon)
+		private readonly IConfiguration _configuration;
+		public DiscountRepository(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		public async Task<Coupon> GetDiscount(string productName)
+		{
+			string connectionString = _configuration.GetSection("DatabaseSettings")
+				.GetValue<string>("ConnectionString");
+
+			using var connection = new NpgsqlConnection(connectionString);
+
+			var coupon = await connection.QueryFirstOrDefaultAsync<Coupon>
+				("SELECT * FROM public.coupon WHERE productname = @productName", new { productName = productName });
+
+			if (coupon == null)
+			{
+				return new Coupon()
+				{
+					ProductName = "No Discount",
+					Amount = 0,
+					Description = "No Discount Desc"
+				};
+			}
+
+			return coupon;
+		}
+
+		public async Task<bool> CreateDiscount(Coupon coupon)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<bool> DeleteDiscount(string productName)
+		public async Task<bool> DeleteDiscount(string productName)
 		{
 			throw new NotImplementedException();
 		}
 
-		public Task<Coupon> GetDiscount(string productName)
-		{
-			throw new NotImplementedException();
-		}
-
-		public Task<bool> UpdateDiscount(Coupon coupon)
+		public async Task<bool> UpdateDiscount(Coupon coupon)
 		{
 			throw new NotImplementedException();
 		}
