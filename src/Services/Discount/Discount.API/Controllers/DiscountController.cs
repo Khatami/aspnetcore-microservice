b2f25069a0bb9e79e9fa4a1daa4b33cc8120c52e
hamedkhatami <hamedkhatami@outlook.com>
2022-01-07 19:08:37 +0330

    Feat (DiscountController): Add DiscountController

---
+++ src/Services/Discount/Discount.API/Controllers/DiscountController.cs
@@ -0,0 +1,26 @@
+﻿using Discount.API.Entities;
+using Discount.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Discount.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DiscountController : ControllerBase
+	{
+		private readonly IDiscountRepository _discountRepository;
+
+		public DiscountController(IDiscountRepository discountRepository)
+		{
+			_discountRepository = discountRepository;
+		}
+
+		[HttpGet("{productName")]
+		public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+		{
+			var discount = await _discountRepository.GetDiscount(productName);
+
+			return Ok(discount);
+		}
+	}
+}
