6e172f8ca043fb22cf504984969ad88029af28c2
hamedkhatami <hamedkhatami@outlook.com>
2022-01-08 14:48:20 +0330

    Feat (Postgresql migration)

--- src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
+++ src/Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Npgsql;
 
 namespace Discount.API.Extensions
 {
@@ -10,10 +10,55 @@ namespace Discount.API.Extensions
 			{
 				var serviceProvider = serviceScope.ServiceProvider;
 
-				var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+				var logger = serviceProvider.GetRequiredService<ILogger<TContext>>();
+				using var connection = serviceProvider.GetRequiredService<NpgsqlConnection>();
+
+				try
+				{
+					logger.LogInformation("Migrating postgresql database.");
+
+					connection.Open();
+					using var command = new NpgsqlCommand()
+					{
+						Connection = connection,
+						CommandText = @"
+							CREATE TABLE IF NOT EXISTS coupon
+							(
+								id SERIAL PRIMARY KEY NOT NULL,
+								productname VARCHAR(24) NOT NULL,
+								description Text,
+								amount INT
+							);
+
+							INSERT INTO public.coupon (productname, description, amount)
+							SELECT 'IPhone 10', 'Samsung Discount', 150
+							WHERE NOT EXISTS (SELECT * FROM public.coupon WHERE productname = 'IPhone 10');
+
+							INSERT INTO public.coupon (productname, description, amount)
+							SELECT 'IPhone X', 'IPhone Discount', 200
+							WHERE NOT EXISTS (SELECT * FROM public.coupon WHERE productname = 'IPhone X');
+						"
+					};
+
+					command.ExecuteNonQuery();
+
+					logger.LogInformation("Migrated postgresql database.");
+				}
+				catch (NpgsqlException ex)
+				{
+					logger.LogError(ex, "an error occurred while migrating the postgresql database");
+
+					if (retryAvailablility < 5)
+					{
+						retryAvailablility++;
+						Thread.Sleep(2000);
+
+						MigrateDatabase<TContext>(host, retryAvailablility);
+					}
+				}
 			}
 
-			return null;
+			return host;
 		}
 	}
 }
