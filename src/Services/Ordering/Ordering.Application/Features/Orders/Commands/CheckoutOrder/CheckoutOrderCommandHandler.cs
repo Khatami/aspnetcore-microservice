721c261b2475ff78607e8cece90eacda840b384c
hamedkhatami <hamedkhatami@outlook.com>
2022-02-02 11:01:04 +0330

    feat (CheckoutOrderCommandHander)

--- src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -3,6 +3,8 @@ using MediatR;
 using Microsoft.Extensions.Logging;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Models;
+using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
 {
@@ -24,9 +26,20 @@ namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
 			_logger = logger;
 		}
 
-		public Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+		public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			var orderEntity = _mapper.Map<Order>(request);
+			var newOrder = await _orderRepository.AddAsync(orderEntity);
+
+			_logger.LogInformation($"Order {newOrder.Id} is successfully created.");
+
+			await SendMail(newOrder);
+
+			return newOrder.Id;
+		}
+
+		private async Task SendMail(Order order)
+		{
 		}
 	}
 }
