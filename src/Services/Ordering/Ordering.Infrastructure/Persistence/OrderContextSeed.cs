a0d99d16029cff03bfa45968431958809cf1cf88
hamedkhatami <hamedkhatami@outlook.com>
2022-02-06 16:36:30 +0330

    feat (Ordering.Infrastructure): add OrderContext and OrderContextSeed

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+	internal class OrderContextSeed
+	{
+		internal static async Task SeedAsync(OrderContext context, ILogger<OrderContextSeed> logger)
+		{
+			if (context.Orders.Any() == false)
+			{
+				await context.Orders.AddRangeAsync(new List<Order>
+				{
+					new Order()
+					{
+						UserName = "swn",
+						FirstName = "Seyedhamed",
+						LastName = "Khatami",
+						EmailAddress = "hamedkhatami@outlook.com"
+					}
+				});
+
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext));
+
+				await context.SaveChangesAsync();
+			}
+		}
+	}
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Respositories/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+using System.Linq.Expressions;
+
+namespace Ordering.Infrastructure.Respositories
+{
+	public class RepositoryBase<T> : IAsyncRepository<T> where T : EntityBase
+	{
+		private readonly OrderContext _orderContext;
+
+		public RepositoryBase(OrderContext orderContext)
+		{
+			_orderContext = orderContext;
+		}
+
+		public Task<IReadOnlyList<T>> GetAllAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> AddAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task DeleteAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null, bool disableTracking = true)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<T> GetByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateAsync(T entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
