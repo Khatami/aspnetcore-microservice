60e92eb390574e144ad7380063bc06f96c3f7ed6
hamedkhatami <hamedkhatami@outlook.com>
2022-03-09 13:59:19 +0330

    feat (Ordering.API): add basketcheckout consumer

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,3 +1,7 @@
+using EventBus.Messages.Common;
+using EventBus.Messages.Events;
+using MassTransit;
+using Ordering.API.EventBusConsumer;
 using Ordering.API.Extensions;
 using Ordering.Application;
 using Ordering.Infrastructure;
@@ -13,6 +17,23 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// MassTransit - RabbitMQ Configuration
+builder.Services.AddMassTransit(config =>
+{
+	config.AddConsumer<BasketCheckoutConsumer>();
+
+	config.UsingRabbitMq((context, config) =>
+	{
+		config.Host(builder.Configuration["EventBusSettings:HostAddress"]);
+
+		config.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, configure =>
+		{
+			configure.ConfigureConsumer<BasketCheckoutConsumer>(context);
+		});
+	});
+});
+builder.Services.AddMassTransitHostedService();
 
 var app = builder.Build();
 app.MigrateDatabase<OrderContext>((context, services) =>
